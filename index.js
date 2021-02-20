ced8bfa21c2cd66bf810dc70275f0499e5ebba19
Cynderian <thispersonlikespie@gmail.com>
2021-02-19 19:09:48 -0500

    overhaul of sphere command

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-console */
 /* eslint-disable no-plusplus */
 const fetch = require('node-fetch');
+const fs = require('fs');
+const split = require('split');
+const { exec } = require('child_process');
 const columnify = require('columnify');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
@@ -44,131 +48,17 @@ function popToCC(pop) { // CC given per system population
   return 11;
 }
 
-async function findSphere(urlsys, message) {
-  let refSysFlag = 0;
-  let page = 1;
-  let total = 0;
-  let idealSystems = 0;
-  let lastResult;
-  const systemData = [];
-  const contestedData = [];
-  let sysCC;
-  let input;
-  let freeCC = 0;
-  const url = `https://elitebgs.app/api/ebgs/v5/systems?sphere=true&factionDetails=true&referenceDistance=15&referenceSystem=${urlsys}`;
-  do {
-    try {
-      console.log(`function page ${page}`);
-      const response = await fetch(`${url}&page=${page}`);
-      const ebgsData = await response.json();
-      if (ebgsData.total === 0) { // if system entered does not exist
-        message.channel.send('Something went wrong; was there a typo?');
-        console.log('command aborted');
-        return;
-      }
-
-      lastResult = ebgsData.hasNextPage;
-      let i = 0;
-      do {
-        console.log(`data fill ${i}`);
-        // Check for special systems, excluded from PP/BGS
-        if (ebgsData.docs[i].name !== 'Shinrarta Dezhra' // you know where this is
-          && ebgsData.docs[i].name !== 'Azoth' // 10 starter systems
-          && ebgsData.docs[i].name !== 'Dromi'
-          && ebgsData.docs[i].name !== 'Lia Fall'
-          && ebgsData.docs[i].name !== 'Matet'
-          && ebgsData.docs[i].name !== 'Orna'
-          && ebgsData.docs[i].name !== 'Otegine'
-          && ebgsData.docs[i].name !== 'Sharur'
-          && ebgsData.docs[i].name !== 'Tarnkappe'
-          && ebgsData.docs[i].name !== 'Tyet'
-          && ebgsData.docs[i].name !== 'Wolfsegen') {
-          total++; // increment for every system that passes the filter
-          const eddbResponse = await fetch(`https://eddbapi.kodeblox.com/api/v4/populatedsystems?name=${ebgsData.docs[i].name}`);
-          const eddbData = await eddbResponse.json();
-
-          if (i === 0 && refSysFlag === 0) { // Store first system name for capitalization purposes
-            input = ebgsData.docs[0].factions[0].faction_details.faction_presence.system_name;
-            refSysFlag++;
-          }
-
-          sysCC = popToCC(ebgsData.docs[i].population);
-
-          const system = {};
-          const contestedSys = {};
-          system.name = ebgsData.docs[i].name;
-          system.government = capitalize((ebgsData.docs[i].government).slice(12, -1));
-          system.lead = infLead(ebgsData, i);
-          system.gov_date = capitalize(`${(ebgsData.docs[i].updated_at).slice(5, 7)}/${(ebgsData.docs[i].updated_at).slice(8, 10)}`);
-          system.cc = sysCC;
-          if (eddbData.docs[0].power != null) { // So null values do not go to capitalize
-            system.power = capitalize(eddbData.docs[0].power);
-            system.state = capitalize(eddbData.docs[0].power_state);
-            if (eddbData.docs[0].power_state === 'exploited') { // If exploited system
-              contestedSys.power = capitalize(eddbData.docs[0].power);
-              contestedSys.cc = sysCC;
-              contestedData.push(contestedSys);
-            }
-          } else {
-            system.power = null;
-            if (eddbData.docs[0].power_state === 'contested') {
-              system.state = capitalize(eddbData.docs[0].power_state);
-              total--;
-            } else {
-              system.state = null;
-              freeCC += sysCC;
-            }
-          }
-          system.pow_date = capitalize(`${(eddbData.docs[0].updated_at).slice(5, 7)}/${(eddbData.docs[0].updated_at).slice(8, 10)}`);
-          systemData.push(system);
-
-          if ((ebgsData.docs[i].government).slice(12, -1) === 'corporate' && eddbData.docs[0].power_state !== 'contested') { idealSystems++; } // Find # of ideal systems
-        }
-        i++;
-      } while (ebgsData.docs[i]);
-      page++;
-    } catch (err) {
-      console.error(`Error: ${err}`);
-    }
-  } while (lastResult);
-
-  // convert contested CC values into readable strings
-  contestedData.sort((a, b) => { // Sort by power alphabetically
-    const nameA = a.power;
-    const nameB = b.power;
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    return 0;
-  });
-  let powerName = contestedData[0].power;
-  let contestedCCStr = '\n';
-  let contestedCC = 0;
-  for (let i = 0; i < contestedData.length; i++) { // Add CC by power
-    if (powerName === contestedData[i].power) {
-      contestedCC += contestedData[i].cc;
-    } else {
-      contestedCCStr = `${contestedCCStr + contestedCC}CC is contested with ${powerName}\n`;
-      powerName = contestedData[i].power;
-      contestedCC = 0;
-    }
-  }
-  contestedCCStr = `${contestedCCStr + contestedCC}CC is contested with ${powerName}\n`; // Needed for last iteration
-
-  const columns = columnify(systemData);
-  console.log('message sent');
-  message.channel.send(`\`\`\`ini\n[${input} Control Sphere Analysis]\n\n${columns}\n\`\`\``); // tabularize info
-  message.channel.send(`\`\`\`\n${idealSystems}/${total - 1} favorable systems for Aisling expansion` // favorable systems footer
-    + `\n${freeCC}CC gained by Aisling expansion${// CC gain footer
-      contestedCCStr // CC contested footer
-    }\n\`\`\``);
-}
-
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}`);
+  exec('wget -N https://eddb.io/archive/v6/systems_populated.jsonl', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+  }); // TODO: create alarm so this updates daily
 });
 
 client.on('message', (message) => {
@@ -193,19 +83,6 @@ client.on('message', (message) => {
         console.log(`Error: ${e.message}`);
       });
   } else if (command === 'sphere') {
-    console.log('Working on sphere...');
-    message.channel.send('Calculating...');
-    let input = '';
-    if (!args.length) { // take all input after sphere and designate it the target system
-      return message.channel.send('Please define a reference system.');
-    } if (args.length > 1) {
-      input = args[0];
-      for (let i = 1; i < args.length; i++) { input = `${input} ${args[i]}`; }
-    } else input = args[0];
-    console.log(`input: ${input}`);
-
-    findSphere(input, message);
-  } else if (command === 'sphere2') {
     console.log('Working...');
     message.channel.send('Calculating...');
     let input = '';
@@ -215,39 +92,156 @@ client.on('message', (message) => {
       input = args[0];
       for (let i = 1; i < args.length; i++) { input = `${input} ${args[i]}`; }
     } else input = args[0];
-    console.log(`input: ${input}`);
-
     const ebgsPromises = [];
-    const eddbPromises = [];
     fetch(`https://elitebgs.app/api/ebgs/v5/systems?sphere=true&factionDetails=true&referenceDistance=15&referenceSystem=${input}`)
       .then((response) => response.json())
-      .then((json) => {
-        for (let i = 1; i < json.pages + 1; i++) { // page # starts at 1
-          ebgsPromises.push(fetch(`https://elitebgs.app/api/ebgs/v5/systems?sphere=true&factionDetails=true&referenceDistance=15&referenceSystem=${input}&page=${i}`));
+      .then((json) => { // find # of pages
+        console.log('got response');
+        if (json.total === 0) { // if system entered does not exist
+          message.channel.send('Something went wrong; was there a typo?');
+          console.log('command aborted');
+          return;
+        }
+        for (let i = 1; i < json.pages + 1; i++) { // page starts at 1
+          const delay = i * 500;
+          const promise = new Promise((resolve) => setTimeout(resolve, delay)).then(() => fetch(`https://elitebgs.app/api/ebgs/v5/systems?sphere=true&factionDetails=true&referenceDistance=15&referenceSystem=${input}&page=${i}`));
+          ebgsPromises.push(promise);
         }
         Promise.all(ebgsPromises)
           .then((responses) => Promise.all(responses.map((response) => response.json())))
           .then((data) => {
             let i = 0;
+            let total = 0;
+            let freeCC = 0;
+            let idealSystems = 0;
+            const lead = [];
             const systemData = [];
+            const exploitedData = [];
+            let refSys; // reference system
             do { // Iterate through each page
               let j = 0;
               do { // Iterate through each system
-                const system = {};
-                console.log(data[i].docs[j].name);
-                system.name = data[i].docs[j].name;
-                systemData.push(system);
-                eddbPromises.push(fetch(`${data[i].docs[j].name}`));
-                j++;
+                // Check for special systems, excluded from PP/BGS
+                if (data[i].docs[j].name !== 'Shinrarta Dezhra' // you know where this is
+                && data[i].docs[j].name !== 'Azoth' // 10 starter systems
+                && data[i].docs[j].name !== 'Dromi'
+                && data[i].docs[j].name !== 'Lia Fall'
+                && data[i].docs[j].name !== 'Matet'
+                && data[i].docs[j].name !== 'Orna'
+                && data[i].docs[j].name !== 'Otegine'
+                && data[i].docs[j].name !== 'Sharur'
+                && data[i].docs[j].name !== 'Tarnkappe'
+                && data[i].docs[j].name !== 'Tyet'
+                && data[i].docs[j].name !== 'Wolfsegen') {
+                  total++; // increment total trigger counting systems
+                  if (i === 0 && j === 0) { // Store first system name for capitalization purposes
+                    // eslint-disable-next-line max-len
+                    refSys = data[i].docs[j].factions[0].faction_details.faction_presence.system_name;
+                  } else { // skips reference system
+                    // console.log(`reading system ${i}:${j}`); // debug
+                    const system = {};
+                    system.name = data[i].docs[j].name;
+                    system.id = data[i].docs[j].eddb_id;
+                    systemData.push(system); // push object with names to array
+                    // assign these later to maintain order for columnify
+                    lead.push(infLead(data[i], j));
+                  }
+                }
+                j++; // increment system selection
               } while (data[i].docs[j]);
               i++;
             } while (data[i]);
+            total--; // reduce by 1 to account for the overincrement in the loop
+            i = 0; // reset for readStream loop
+            systemData.sort((a, b) => a.id - b.id); // sorts systems by ID lowest to highest
+            console.log('reading local file');
+            fs.createReadStream('./systems_populated.jsonl')
+              .pipe(split(JSON.parse))
+              .on('data', (obj) => { // this iterates through every system
+                // TODO: find a way around manually removing the final space on systems_population,
+                // so theres not an unexpected json file end
+                if (systemData[i] != null) {
+                  if (obj.id === systemData[i].id) {
+                    // Convert Unix time to UTC
+                    let lastUpdated = new Date(obj.updated_at * 1000).toISOString(); // set to UTC
+                    lastUpdated = `${lastUpdated.slice(5, 7)}/${lastUpdated.slice(8, 10)}`; // reformat
+
+                    // Account for exploited systems
+                    const exploitedSystem = {};
+                    if (obj.power_state === 'Exploited') { // If exploited system
+                      exploitedSystem.power = obj.power;
+                      exploitedSystem.cc = popToCC(obj.population);
+                      exploitedData.push(exploitedSystem);
+                      total--; // to be contested systems will not count towards triggers
+                    } else if (obj.power_state === 'Contested' || obj.power_state === 'Control') {
+                      total--; // contested systems do not count towards triggers
+                    } else {
+                      freeCC += popToCC(obj.population);
+                      if (obj.government === 'Corporate') {
+                        idealSystems++;
+                      }
+                    }
+
+                    // Add data to object array
+                    systemData[i].government = obj.government;
+                    systemData[i].lead = lead[i];
+                    systemData[i].cc = popToCC(obj.population);
+                    systemData[i].power = obj.power;
+                    systemData[i].state = obj.power_state;
+                    systemData[i].date = lastUpdated;
+                    i++;
+                  }
+                }
+              })
+              .on('close', () => { // acting as .then for createReadStream
+                i = 0;
+                while (systemData[i]) {
+                  delete systemData[i].id;
+                  i++;
+                }
+                // convert contested CC values into readable strings
+                // Sort by power alphabetically
+                exploitedData.sort((a, b) => {
+                  const nameA = a.power;
+                  const nameB = b.power;
+                  if (nameA < nameB) { return -1; }
+                  if (nameA > nameB) { return 1; }
+                  return 0;
+                });
+                // convert to readable string
+                let exploitedCCStr = '';
+                if (exploitedData.length !== 0) {
+                  let powerName = exploitedData[0].power; // set initial power
+                  exploitedCCStr = '\n';
+                  let exploitedCC = 0;
+                  for (i = 0; i < exploitedData.length; i++) { // Add CC by power
+                    if (powerName === exploitedData[i].power) {
+                      exploitedCC += exploitedData[i].cc;
+                    } else {
+                      exploitedCCStr = `${exploitedCCStr + exploitedCC}CC will be contested with ${powerName}\n`;
+                      powerName = exploitedData[i].power;
+                      exploitedCC = exploitedData[i].cc;
+                    }
+                  }
+                  exploitedCCStr = `${exploitedCCStr + exploitedCC}CC will be contested with ${powerName}\n`; // Needed for last iteration
+                }
+                const columns = columnify(systemData); // tabularize info
+                message.channel.send(`\`\`\`ini\n[${refSys} Control Sphere Analysis]\n\n${columns}\n\`\`\``);
+                message.channel.send(`\`\`\`\n${idealSystems}/${total} favorable systems for Aisling expansion` // favorable systems footer
+                + `\n${freeCC}CC gained by Aisling expansion${// CC gain footer
+                  exploitedCCStr // CC contested footer
+                }\n\`\`\``);
+                console.log('command done');
+              })
+              .on('error', (err) => {
+                console.log(err);
+              });
           })
-          .catch((error) => {
-            console.log(error);
-          });
+          .catch((err) => { console.log(`Promise problem: ${err.message}`); });
       })
-      .catch((err) => { console.log(`Fetch problem: ${err.message}`); });
+      .catch((err) => {
+        console.log(`Fetch problem: ${err.message}`);
+      });
   }
 });
 client.login(token);
